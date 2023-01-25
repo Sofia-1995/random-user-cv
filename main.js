2e588f01c87514cce2585e9edae256f8717de3b4
Sofi <sofi@MacBook-Air-de-Sofi.local>
2023-01-25 02:54:20 +0100

    Agrego logica de botones

---
+++ main.js
@@ -0,0 +1,22 @@
+function mostrarNombre() {
+  document.getElementById("nombre").innerHTML = "Misty Bryant";
+  document.getElementById("etiqueta").innerHTML = "Mi nombre es:";
+}
+
+function mostrarMail() {
+  document.getElementById("nombre").innerHTML = "gladys.chambers@example.com";
+  document.getElementById("etiqueta").innerHTML = "Mi mail es:";
+}
+
+function mostrarCumpleanos() {
+  document.getElementById("nombre").innerHTML = "1/2/1988";
+  document.getElementById("etiqueta").innerHTML = "Mi cumpleaños es:";
+}
+function mostrarLocalizacion() {
+  document.getElementById("nombre").innerHTML = "7003 Westheimer Rd";
+  document.getElementById("etiqueta").innerHTML = "Mi dirección es:";
+}
+function mostrarTelefono() {
+  document.getElementById("nombre").innerHTML = "(764) 763-1207";
+  document.getElementById("etiqueta").innerHTML = "Mi teléfono es:";
+}
